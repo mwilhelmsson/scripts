82c2de45a11d9112a94734d55b4cf254417755cf
Mikael Wilhelmsson <mikael.wilhelmsson@gmail.com>
2015-11-16 19:52:14 +0100

    intial commit with password generator

---
+++ pwdgen.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+pwdlen=0
+if [ $# -eq 0 ]
+then
+    echo "How long should the password be?"
+    read pwdlen
+else
+    pwdlen=$1
+fi
+
+if [ $pwdlen -lt 8 ]
+then
+    pwdlen=8
+fi
+
+cat /dev/urandom | tr -dc 'a-zA-Z0-9-_!@#$%^&*()_+{}|:<>?=' | fold -w $pwdlen | head -n 4 | grep -i '[!@#$%^&*()_+{}|:<>?=]'
